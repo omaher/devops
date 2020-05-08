0f69d17ab7541308f4e7b42973bb392112315f98
ahero <ahero@DESKTOP-220BC8K>
2020-05-08 17:32:43 +0530

    second commit

--- Website/src/main/java/Website/HelloWorld.java
+++ Website/src/main/java/Website/HelloWorld.java
@@ -4,8 +4,17 @@ package Website;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
+		
+		int i ;
+		int y;
+	
+		
 		System.out.println("Hello World , This is my first job");
+		add(5,6);
+	}
+	
+	static void add(int a,int b) {
+		System.out.println(a+b);
 	}
 
 }
